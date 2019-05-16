89344a40865a7e780db672ef6bb3ff6f75dd2eba
Shterion <shterion.yanev@yahoo.com>
2019-05-16 15:27:47 +0200

    Enable tests

--- test/triggers.test.js
+++ test/triggers.test.js
@@ -17,32 +17,31 @@ describe('Triggers - getPersons & getOrganizations', () => {
     getOrganizationsEmpty;
   });
 
-  // it('should get all persons', async () => {
-  //   const snapshot = {
-  //     lastUpdated: (new Date(0)).getTime(),
-  //     currentPage: 1,
-  //   };
-  //   const persons = await getPersons(token, snapshot);
-  //   expect(persons.result).to.not.be.empty;
-  //   expect(persons.result).to.be.a('array');
-  //   expect(persons.result).to.have.length(2);
-  //   expect(persons.result[0].firstName).to.equal('John');
-  //   expect(persons.result[0].lastName).to.equal('Doe');
-  //   expect(persons.result[0].photo).to.equal('www.photo.com/john');
-  //   expect(persons.result[0].addresses[0].street).to.equal('Main Str');
-  //   expect(persons.result[0].addresses[0].streetNumber).to.equal('688');
-  //   expect(persons.result[0].addresses[0].city).to.equal('New York City');
-  //   expect(persons.result[0].contactData[0].value).to.equal('johny@mail.com');
-  //   expect(persons.result[0].contactData[2].type).to.equal('linkedIn');
-  // });
+  it('should get all persons', async () => {
+    const snapshot = {
+      lastUpdated: (new Date(0)).getTime(),
+    };
+    const persons = await getPersons(token, snapshot);
+    expect(persons.result).to.not.be.empty;
+    expect(persons.result).to.be.a('array');
+    expect(persons.result).to.have.length(2);
+    expect(persons.result[0].firstName).to.equal('John');
+    expect(persons.result[0].lastName).to.equal('Doe');
+    expect(persons.result[0].photo).to.equal('www.photo.com/john');
+    expect(persons.result[0].addresses[0].street).to.equal('Main Str');
+    expect(persons.result[0].addresses[0].streetNumber).to.equal('688');
+    expect(persons.result[0].addresses[0].city).to.equal('New York City');
+    expect(persons.result[0].contactData[0].value).to.equal('johny@mail.com');
+    expect(persons.result[0].contactData[2].type).to.equal('linkedIn');
+  });
 
-  // it('should throw an exception if no persons were found', async () => {
-  //   const snapshot = {
-  //     lastUpdated: 0,
-  //   };
-  //   const persons = await getPersons(token, snapshot);
-  //   expect(persons).to.equal('Expected records array.');
-  // });
+  it('should throw an exception if no persons were found', async () => {
+    const snapshot = {
+      lastUpdated: 0,
+    };
+    const persons = await getPersons(token, snapshot);
+    expect(persons).to.equal('Expected records array.');
+  });
 
   it('should get all organizations', async () => {
     const snapshot = {
